df1d5ed3b82314f474ba9a0eb353e5b5012bb80e
StanDarius24 <standarius24@gmail.com>
2022-07-10 23:42:51 +0300

    initial commit

---
+++ src/main/kotlin/com/tuts/tutsrestfulspringjpaaws/TutsRestfulSpringJpaAwsApplication.kt
@@ -0,0 +1,10 @@
+package com.tuts.tutsrestfulspringjpaaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class TutsRestfulSpringJpaAwsApplication
+
+fun main(args: Array<String>) {
+  runApplication<TutsRestfulSpringJpaAwsApplication>(*args)
+}
---
+++ src/test/kotlin/com/tuts/tutsrestfulspringjpaaws/TutsRestfulSpringJpaAwsApplicationTests.kt
@@ -0,0 +1,10 @@
+package com.tuts.tutsrestfulspringjpaaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutsRestfulSpringJpaAwsApplicationTests {
+
+  @Test fun contextLoads() {}
+}
